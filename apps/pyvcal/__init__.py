d04fd4f6df000611171bcc41c00a2444161ff778
Harley Bussell <harley@zealphp.com>
2010-01-31 02:02:04 -0600

    fix requirements typo

--- apps/pyvcal/__init__.py
+++ apps/pyvcal/__init__.py
@@ -17,7 +17,7 @@ get_api(...).Repository
 from repository import Repository
 
 #import perforce, subversion, git_wrapper as git
-import subversion, git_wrapper as git
+import git_wrapper as git
 
 USAGE_STRING = """
 Usage: vcs_api = pyvcal.get_api('<vcs>')
@@ -32,7 +32,7 @@ def get_api(system):
     try:
         vcs = {
             'git' : git,
-            'subversion' : subversion,
+            #'subversion' : subversion,
             #'perforce' : perforce
         }[system]
     except KeyError, e:
